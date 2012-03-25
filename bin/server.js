20b7b720d868b3c7463ae0d3536b92c1abb58d7e
Tim Baumann <tim@timbaumann.info>
2012-03-25 23:20:46 +0200

    CodeMirror demo

---
+++ bin/server.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var ot = require('../lib/operational-transformation');
+var Server = require('../lib/server').Server;
+var express = require('express');
+var socketIO = require('socket.io');
+var path = require('path');
+
+var app = express.createServer();
+
+app.configure(function () {
+  app.use(express.logger());
+  app.use(express.static(path.join(__dirname, '../integration')));
+  app.use(express.static(path.join(__dirname, '../lib')));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+var io = socketIO.listen(app);
+
+var str = "lorem ipsum\ndolor sit amet";
+
+var server = new Server(str);
+
+server.addListener('newOperation', function (operation) {
+  io.sockets.emit('operation', operation);
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('doc', { str: server.str, revision: server.operations.length });
+  socket.on('operation', function (operation) {
+    operation = ot.Operation.fromJSON(operation);
+    server.receiveOperation(operation);
+    console.log("new operation: " + operation);
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Listening on port 3000");
+});
---
+++ integration/codemirror/client.js
@@ -0,0 +1,101 @@
+(function () {
+  function codeMirrorChangeToOperation (operation, cm, change, oldValue) {
+    var newValue = cm.getValue();
+    var from = cm.indexFromPos(change.from);
+    var text = change.text.join('\n');
+    var diff = newValue.length - oldValue.length;
+    var deletedChars = text.length - diff;
+
+    if (from > 0) {
+      operation.retain(from);
+    }
+    if (deletedChars > 0) {
+      operation.delete(oldValue.slice(from, from + deletedChars));
+    }
+    if (text) {
+      operation.insert(text);
+    }
+    if (oldValue.length - operation.baseLength > 0) {
+      operation.retain(oldValue.length - operation.baseLength);
+    }
+
+    return operation;
+  }
+
+  function codeMirrorApplyOperation (cm, operation) {
+    cm.operation(function () {
+      var ops = operation.ops;
+      var index = 0;
+      for (var i = 0, l = ops.length; i < l; i++) {
+        var op = ops[i];
+        if (op.retain) {
+          index += op.retain;
+        } else if (op.insert) {
+          cm.replaceRange(op.insert, cm.posFromIndex(index));
+          index += op.insert.length;
+        } else if (op.delete) {
+          var from = cm.posFromIndex(index);
+          var to   = cm.posFromIndex(index + op.delete.length);
+          assert(cm.getRange(from, to) === op.delete);
+          cm.replaceRange('', from, to);
+        }
+      }
+      assert(index === cm.getValue().length);
+    });
+  }
+
+  function assert (b, msg) {
+    if (!b) {
+      throw new Error(msg || "assertion error");
+    }
+  }
+
+  var Client = ot_client.Client;
+
+  var socket = io.connect('http://localhost');
+  socket.on('doc', function (obj) {
+    initialize(obj.str, obj.revision);
+  });
+
+  function initialize (str, revision) {
+    var client = new Client(revision);
+
+    client.sendOperation = function (operation) {
+      // uncomment to simulate more latency
+      //setTimeout(function () {
+        socket.emit('operation', operation);
+      //}, 500);
+    };
+
+    var fromServer = false;
+    client.applyOperation = function (operation) {
+      fromServer = true;
+      codeMirrorApplyOperation(cm, operation);
+      fromServer = false;
+    };
+
+    var wrapper = document.getElementById('wrapper');
+    var oldValue = str;
+    var cm = CodeMirror(wrapper, {
+      lineNumbers: true,
+      //mode: 'javascript',
+      value: str,
+      onChange: function (cm, change) {
+        if (fromServer) {
+          oldValue = cm.getValue();
+        } else {
+          var operation = client.createOperation();
+          var operation = codeMirrorChangeToOperation(operation, cm, change, oldValue);
+          console.log(change, operation);
+          client.applyClient(operation);
+          oldValue = cm.getValue();
+        }
+      }
+    });
+
+    socket.on('operation', function (operation) {
+      operation = operational_transformation.Operation.fromJSON(operation);
+      client.applyServer(operation);
+    });
+  }
+})();
